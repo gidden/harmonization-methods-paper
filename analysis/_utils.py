1d326ebea3e96cf870b9245c96b81e8e42d60f7f
Matthew Gidden <matthew.gidden@gmail.com>
2017-05-19 11:15:02 +0200

    about to rm files

---
+++ analysis/_utils.py
@@ -0,0 +1,7 @@
+import sys
+
+pth = '../../../iiasa/ceds_harmonization_analysis/'
+sys.path.append(pth)
+
+
+from utils import *
--- analysis/make_results.py
+++ analysis/make_results.py
@@ -1,11 +1,16 @@
-import sys
 import pandas as pd
-sys.path.append('../../../iiasa/ceds_harmonization_analysis/')
-import utils
 
-x = utils.read_data('45-no-overrides_harmonized', ext='xlsx')
-y = utils.read_data(
-    'MESSAGE-GLOBIOM_SSP2-45-SPA2-V25_unharmonized_harmonized', ext='xlsx')
-z = pd.read_csv('read_data_just_ceds.csv')
+import _utils as utils
 
-pd.concat([x, y, z]).to_csv('results.csv', index=False)
+# read in latest snapshot, get only ceds and message results
+x = pd.read_csv(utils.pth + 'v25.csv')
+lower = x.REGION == x.REGION.str.lower()  # rm isos
+x = x[~lower]
+x = x[(x.SCENARIO == 'CEDS') | (x.MODEL.str.contains('MESSAGE'))]
+
+# you have to dump to csv first because read_data only reads files for now
+x.to_csv('results_v25.csv', index=False)
+y = utils.read_data('results_v25')
+z = utils.read_data('45-no-overrides_harmonized', ext='xlsx')
+z.columns = [str(_) for _ in z.columns]
+pd.concat([y, z]).to_csv('results.csv', index=False)
